9e969f508c534bc028efd209d386ae652d87abe1
Elisa Vaccaro <erv230@nyu.edu>
2017-09-14 03:14:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 function theBeatlesPlay(arr1, arr2){
   var answer = [];
   
-  for(var i = 0; i < arr1.length; i++){
-    for(var j = 0; j < arr2.length; j++){
-      answer.push(`${arr1[i]} plays ${arr2[j]}`);
+  for(var i = 0; i < arr1.length; i++)
+      answer.push(`${arr1[i]} plays ${arr2[i]}`);
     }
-  }
   return answer;
 }
